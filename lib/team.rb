ceaaade190be3dac01fc01741884c75e5c27fa00
Alex Kiselich <alexander.kiselich@gmail.com>
2023-03-29 10:04:32 -0600

    Add roster, player count, and add player method

--- lib/team.rb
+++ lib/team.rb
@@ -1,6 +1,16 @@
 class Team
-  attr_reader :roster
+  attr_reader :roster, :player_count
   def initialize(name, city)
     @roster = []
+    @player_count = 0
   end
+
+  def add_player(player)
+    @player_count += 1
+    @roster << player
+  end
+
+
+
+
 end
